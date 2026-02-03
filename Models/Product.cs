d660b9f9579a2414a5a12b1b69c40367a00b9e7f
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-02-03 12:16:41 +0100

    chore: add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace FakeStoreProxyApi.Models;
+
+public record Rating(decimal Rate, int Count);
+
+public record Product
+(
+    int Id,
+    string Title,
+    decimal Price,
+    string Description,
+    string Category,
+    string Image,
+    Rating Rating
+);
